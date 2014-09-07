0d54379072288ed6c54b6f4060e878ff2afbd6ac
Andrée Brazeau <abrazeau@bitcasa.com>
2014-09-06 20:33:31 -0700

    ok I think it's fix now

--- disrupt_marriage.rb
+++ disrupt_marriage.rb
@@ -13,16 +13,19 @@ EOF
 end
 
 post '/process' do
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Expose-Headers'] = 'ETag'
+  headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+  headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+  headers['Access-Control-Max-Age'] = '86400'
 
   {score: 95}.to_json
 end
 
-
-options "*" do
-  response.headers["Allow"] = "HEAD,GET,PUT,DELETE,OPTIONS"
-
-  # Needed for AngularJS
-  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-
-  halt HTTP_STATUS_OK
+get '*' do
+  response.headers['Allows'] = "HEAD,GET,PUT,DELETE,OPTIONS"
+  # Needed for AngularJS
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+  halt 200
 end
