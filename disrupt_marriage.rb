df130fe14d0523c6314d3841fb16329e8c83fb69
Andrée Brazeau <abrazeau@bitcasa.com>
2014-09-06 19:43:10 -0700

    moer

--- disrupt_marriage.rb
+++ disrupt_marriage.rb
@@ -13,11 +13,12 @@ EOF
 end
 
 post '/process' do
-  response.headers["Access-Control-Allow-Origin"] = "*"
-  response.headers['Access-Control-Expose-Headers'] = 'ETag'
-  response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-  response.headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-  response.headers['Access-Control-Max-Age'] = '86400'
+  response['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    headers['Access-Control-Max-Age'] = '86400'
 
   {score: 95}.to_json
 end
