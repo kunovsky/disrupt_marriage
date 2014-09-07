0588ea5f273045dc5441266e677bc176030a93e3
Andrée Brazeau <abrazeau@bitcasa.com>
2014-09-06 19:40:26 -0700

    add more headers.

--- disrupt_marriage.rb
+++ disrupt_marriage.rb
@@ -14,7 +14,10 @@ end
 
 post '/process' do
   response.headers["Access-Control-Allow-Origin"] = "*"
-  response['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Expose-Headers'] = 'ETag'
+  response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+  response.headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+  response.headers['Access-Control-Max-Age'] = '86400'
 
   {score: 95}.to_json
 end
