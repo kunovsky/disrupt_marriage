e88a35abfd80971cba192a7d7cb2485fc379b901
Andrée Brazeau <abrazeau@bitcasa.com>
2014-09-06 20:37:45 -0700

    oups need options back

--- disrupt_marriage.rb
+++ disrupt_marriage.rb
@@ -17,7 +17,7 @@ post '/process' do
   response.headers['Access-Control-Allow-Origin'] = '*'
   response.headers['Access-Control-Expose-Headers'] = 'ETag'
   response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-  response.headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
   response.headers['Access-Control-Max-Age'] = '86400'
 
   {score: 95}.to_json
@@ -26,6 +26,12 @@ end
 options '/*' do
   response.headers['Allows'] = "HEAD,GET,PUT,DELETE,OPTIONS"
   # Needed for AngularJS
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Expose-Headers'] = 'ETag'
+  response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+  response.headers['Access-Control-Max-Age'] = '86400'
+
   response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
   halt 200
 end
