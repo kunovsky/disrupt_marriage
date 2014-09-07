b69f78abc449f03da98f772b5c68b57798f2c0e1
Tyson Kunovsky <tyson@gottman.com>
2014-09-06 18:06:18 -0700

    adding cors

--- disrupt_marriage.rb
+++ disrupt_marriage.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 require 'thin'
 require 'json'
 
+
+before do
+  if request.request_method == 'OPTIONS'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "POST"
+
+    halt 200
+  end
+end
+
 get '/' do
 <<EOF
   <html><head><title>Here Is The Page</title></head>
@@ -11,7 +21,8 @@ get '/' do
 EOF
 end
 
-post '/process' do
+get '/process' do
+
 {score: 95}.to_json
 end
 
