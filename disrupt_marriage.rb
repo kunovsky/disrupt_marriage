0baf5119ca6d95f77c97fd6dcb2d2ee03105a3aa
Tyson Kunovsky <tyson@gottman.com>
2014-09-06 18:11:00 -0700

    fixing

--- disrupt_marriage.rb
+++ disrupt_marriage.rb
@@ -4,14 +4,6 @@ require 'thin'
 require 'json'
 
 
-before do
-  if request.request_method == 'OPTIONS'
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "POST"
-
-  end
-end
-
 get '/' do
 <<EOF
   <html><head><title>Here Is The Page</title></head>
@@ -21,6 +13,8 @@ EOF
 end
 
 post '/process' do
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers["Access-Control-Allow-Methods"] = "POST"
 
 {score: 95}.to_json
 end
